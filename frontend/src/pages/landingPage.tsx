f47a863ca1ecc4c36f5217222b567295115bb3b4
AlloysiusM <albrentmanlutac@gmail.com>
2025-06-26 19:32:02 +1200

    added logout btn
    still need to fix username not being called correctly

--- frontend/src/pages/landingPage.tsx
+++ frontend/src/pages/landingPage.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Box, TextField, Typography, Button, Divider, useTheme } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
     const[isLoggedIn, setIsLoggedIn] = useState(false);
     const[firstName, setFirstName] = useState('');
+    const navigate = useNavigate();
 
     // Check if user is logged in
     useEffect(() => {
         const token = localStorage.getItem("token");
-        const name = localStorage.getItem('firstName');
+        const name = localStorage.getItem("firstName");
 
         console.log('Token on landing page:', token);
         if(token) {
@@ -19,17 +21,26 @@ function LandingPage() {
         }
     }, []);
 
-    // loutout
-    // cosnt logout
-    // removeitem 
-    // set profile stuff to false or empty string;
+    // logout
+    const logout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("firstName");
+        setIsLoggedIn(false);
+        setFirstName('');
+
+        // logs
+        console.log("Token after removal:", localStorage.getItem("token"));
+        console.log("Name after removal:", localStorage.getItem("firstName"));
+        console.log("Logged in status:", isLoggedIn);
+        navigate('/');
+    }
 
     return (
         <Box>
             {isLoggedIn ? 
             <>
                 <Typography>Welcome back, {firstName}!</Typography>
-                <Button>Log out</Button>
+                <Button onClick={logout}>Log out</Button>
             </> : 
                 <Typography>Please log in</Typography>}
         </Box>
